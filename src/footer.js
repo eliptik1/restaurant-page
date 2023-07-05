9ef8071ad82840944e935401d618d636ce764e34
eliptik <adamfeyk5@gmail.com>
2023-07-05 16:43:54 +0300

    Add footer

--- src/footer.js
+++ src/footer.js
@@ -1,7 +1,10 @@
 function createFooter(){
     const footer = document.createElement("footer")
 
-    footer.textContent = "Footer"
+    footer.innerHTML = `Copyright &copy 2023 eliptik1 &nbsp
+    <a href="https://github.com/eliptik1">
+        <i class="fa fa-github" style="font-size:26px; color: rgb(255, 202, 104)"></i>
+    </a>`
 
     return footer
 }
